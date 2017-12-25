2263772e2327f2c03fb9bee9a0a91f889ee76360
Orelia Sokambi <orelia.sokambi@gmail.com>
2017-12-25 23:14:02 +0100

    modif README + work on app.js -> state

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,20 +9,36 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 /*import {sampleText} from './sampleText';*/
-var sampleText = '# Titre\n## Sous-Titre \n### Titre plus profond\n \nLes paragraphes sont séparés\npar une ligne vide.\n\nLaisser deux espaces à la fin d\'une ligne pour  \naller à la ligne.\n\nAttributs *italique*, **gras**, \n`monospace`, ~~rayé~~.\n\nListe:\n\n  * HTML\n  * CSS\n  * Reactjs\n\nListe numérotée:\n\n  1. Reactjs\n  2. HTML\n  3. CSS\n\n *[Orélia Sokambi](https://github.com/OreliaSk)* \n\n Lien automatique : https://www.linkedin.com/in/orelia-sokambi/ \n\n```\n console.log("Testez vous même cet éditeur en markdown !"); \n``` ';
 
 var MarkdownApp = function (_React$Component) {
 	_inherits(MarkdownApp, _React$Component);
 
-	function MarkdownApp() {
+	function MarkdownApp(props) {
 		_classCallCheck(this, MarkdownApp);
 
-		return _possibleConstructorReturn(this, (MarkdownApp.__proto__ || Object.getPrototypeOf(MarkdownApp)).apply(this, arguments));
+		var sampleText = '# Titre\n## Sous-Titre \n### Titre plus profond\n \nLes paragraphes sont séparés\npar une ligne vide.\n\nLaisser deux espaces à la fin d\'une ligne pour  \naller à la ligne.\n\nAttributs *italique*, **gras**, \n`monospace`, ~~rayé~~.\n\nListe:\n\n  * HTML\n  * CSS\n  * Reactjs\n\nListe numérotée:\n\n  1. Reactjs\n  2. HTML\n  3. CSS\n\n *[Orélia Sokambi](https://github.com/OreliaSk)* \n\n Lien automatique : https://www.linkedin.com/in/orelia-sokambi/ \n\n```\n console.log("Testez vous même cet éditeur en markdown !"); \n``` ';
+
+		var _this = _possibleConstructorReturn(this, (MarkdownApp.__proto__ || Object.getPrototypeOf(MarkdownApp)).call(this, props));
+
+		_this.state = {
+			text: sampleText
+		};
+		return _this;
 	}
 
+	// Functions
+
+
 	_createClass(MarkdownApp, [{
+		key: "editText",
+		value: function editText(e) {
+			var text = e.target.value;
+			console.log(text);
+		}
+	}, {
 		key: "render",
 		value: function render() {
+
 			return React.createElement(
 				"div",
 				{ className: "container" },
@@ -32,12 +48,17 @@ var MarkdownApp = function (_React$Component) {
 					React.createElement(
 						"div",
 						{ className: "col-md-6" },
-						React.createElement("textarea", { rows: "35", value: sampleText, className: "form-control" })
+						React.createElement("textarea", {
+							onChange: this.editText,
+							value: this.state.text,
+							rows: "35",
+							className: "form-control"
+						})
 					),
 					React.createElement(
 						"div",
 						{ className: "col-md-6" },
-						sampleText
+						this.state.text
 					)
 				)
 			);
--- src/app.js
+++ src/app.js
@@ -1,19 +1,40 @@
 /*import {sampleText} from './sampleText';*/
-const sampleText = 
-'# Titre\n## Sous-Titre \n### Titre plus profond\n \nLes paragraphes sont séparés\npar une ligne vide.\n\nLaisser deux espaces à la fin d\'une ligne pour  \naller à la ligne.\n\nAttributs *italique*, **gras**, \n`monospace`, ~~rayé~~.\n\nListe:\n\n  * HTML\n  * CSS\n  * Reactjs\n\nListe numérotée:\n\n  1. Reactjs\n  2. HTML\n  3. CSS\n\n *[Orélia Sokambi](https://github.com/OreliaSk)* \n\n Lien automatique : https://www.linkedin.com/in/orelia-sokambi/ \n\n```\n console.log("Testez vous même cet éditeur en markdown !"); \n``` ';
 
 
 
 class MarkdownApp extends React.Component {
+	
+	constructor(props) {
+		const sampleText = '# Titre\n## Sous-Titre \n### Titre plus profond\n \nLes paragraphes sont séparés\npar une ligne vide.\n\nLaisser deux espaces à la fin d\'une ligne pour  \naller à la ligne.\n\nAttributs *italique*, **gras**, \n`monospace`, ~~rayé~~.\n\nListe:\n\n  * HTML\n  * CSS\n  * Reactjs\n\nListe numérotée:\n\n  1. Reactjs\n  2. HTML\n  3. CSS\n\n *[Orélia Sokambi](https://github.com/OreliaSk)* \n\n Lien automatique : https://www.linkedin.com/in/orelia-sokambi/ \n\n```\n console.log("Testez vous même cet éditeur en markdown !"); \n``` ';
+		super(props);
+		this.state = {
+			text : sampleText
+		}
+	}
+
+	// Functions
+	editText(e){
+		const text = e.target.value;
+		console.log(text);
+	}
+	
+
 	render() {
+
 		return(
 			<div className="container">
 				<div className="row">
 					<div className="col-md-6">
-						<textarea rows="35" value={sampleText} className="form-control"></textarea>
+						<textarea 
+							onChange={this.editText}
+							value={this.state.text} 
+							rows="35" 
+							className="form-control"
+						>
+						</textarea>
 					</div>
 					<div className="col-md-6">
-						{sampleText}
+						{this.state.text}
 					</div>
 				</div>
 			</div>
