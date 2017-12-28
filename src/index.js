af2cddd55cc15d3cfd6324e12414711e3d8352b2
Orelia Sokambi <orelia.sokambi@gmail.com>
2017-12-28 14:57:27 +0100

    index.js ajout localStorage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-//import './style/style.css';
+import './style/style.css';
 import {sampleText} from './sampleText';
 import marked from 'marked';
 
@@ -9,6 +9,18 @@ class MarkdownApp extends React.Component {
 	
 	state = {
 		text : sampleText
+	}
+
+	// Functions components
+	componentWillUpdate(nextProps, nextState) {
+		localStorage.setItem('text', nextState.text);
+	}
+
+	componentWillMount() {
+		const localStorageText = localStorage.getItem('text');
+		if (localStorageText) {
+			this.setState({ text : localStorageText});
+		}
 	}
 
 	// Functions
@@ -25,19 +37,28 @@ class MarkdownApp extends React.Component {
 	render() {
 
 		return(
-			<div className="container">
-				<div className="row">
-					<div className="col-md-6">
-						<textarea 
-							onChange={this.editText}
-							value={this.state.text} 
-							rows="35" 
-							className="form-control"
-						>
-						</textarea>
-					</div>
-					<div className="col-md-6">
-						<div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+			<div>
+				<nav className="navbar navbar-default">
+				  <div className="container-fluid">
+				    <div className="navbar-header">
+				      <a className="navbar-brand">Markdown editor</a>
+				    </div>
+				  </div>
+				</nav>
+				<div className="main container">
+					<div className="row">
+						<div className="col-md-4 col-md-offset-2">
+							<textarea 
+								onChange={this.editText}
+								value={this.state.text} 
+								rows="29" 
+								className="form-control"
+							>
+							</textarea>
+						</div>
+						<div className=" renderText col-md-4">
+							<div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+						</div>
 					</div>
 				</div>
 			</div>
